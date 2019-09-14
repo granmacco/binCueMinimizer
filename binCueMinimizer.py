e12b6913fb264db5006f95fb7d004f2c7b27ca83
Sandor <steamgrabberapp@gmail.com>
2019-09-14 16:19:42 +0200

    Mensaje de error para CUEs con BINs desaparecidos y cambios en el .gitignore

--- binCueMinimizer.py
+++ binCueMinimizer.py
@@ -13,7 +13,6 @@
 # Also, special thanks to @Kactius and @Jcarliman for finding @krcroft's post, 
 # putting me on the right track and inspiring some of the features of this script.
 # https://github.com/kactius
-# https://github.com/jcarliman
 # Thank you very much for sharing your knowledge and for striving for excellence!
 #
 # Lastly, but not least, a very special thanks to @Sergi_0, for providing new ideas and methodologies to this script.
@@ -29,6 +28,7 @@
 import os
 import os.path as osp
 import sys
+import copy
 import subprocess
 from subprocess import call
 from pathlib import Path
@@ -37,9 +37,278 @@ import re
 import logging
 from ntpath import basename
 import platform
+from cueparser import CueSheet
 
 version="1.0"
+# 75 frames por segundo, fuente: https://en.wikipedia.org/wiki/Track_(optical_disc)
+frames_por_segundo = 75
 logging.basicConfig(level=logging.DEBUG, filename="binCueMinimizer.log", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+Blocksizes={
+    'AUDIO'.casefold():      2352,
+    'MODE1/2352'.casefold(): 2352,
+    'MODE2/2352'.casefold(): 2352,
+    'CDI/2352'.casefold():   2352,
+    'CDG'.casefold():        2448,
+    'MODE1/2048'.casefold(): 2048,
+    'MODE2/2336'.casefold(): 2336,
+    'CDI/2336'.casefold():   2336
+    }
+
+class Bin:
+    path = None
+    num = 0
+    blocksize = None
+    is_audio_bin = False
+    
+    def __init__(self, path, num = None):
+        self.path = path
+        if num:
+            self.num = num
+    
+    def is_audio_bin(self):
+        return self.is_audio_bin
+    
+    def is_data_bin(self):
+        return not self.is_audio_bin
+
+class Index:
+    id = None
+    timestamp = None
+    length_in_frames = None
+    
+    regex_line = r'(\d+) (\d+:\d+:\d+)'
+    regex_timestamp = r'(\d+) (\d+):(\d+):(\d+)'
+    compiled_regex_line = re.compile(regex_line)
+    compiled_regex_timestamp = re.compile(regex_timestamp)
+    
+    def __init__(self, line = None, index_id = None, timestamp = None):
+        if line:
+            self.set_from_string(line)
+        elif index_id and timestamp:
+            self.set(index_id, timestamp)
+        
+    def set_from_string(self, line):
+        results = self.compiled_regex_line(line)
+        if results:
+            self.set(results.group(1), results.group(2))
+        
+    def set(self, index_id, timestamp):
+        self.id = index_id
+        self.timestamp = timestamp
+        self.length_in_frames = self.from_timestamp_to_frames(timestamp)
+        
+    def from_timestamp_to_frames(self, timestamp):
+        # timestamp = min:sec:frames
+        results = self.compiled_regex_timestamp.match(timestamp)
+        mins = int(results.group(1))
+        secs = int(results.group(2))
+        frames = int(results.group(3))
+        
+        return frames + (secs * frames_por_segundo) + (mins * 60 * frames_por_segundo)
+        
+
+class Track:
+    id = None
+    path = None
+    num = 0
+    type = None
+    is_audio_track = False
+    indexes = None
+    blocksize = None
+    tracksize = None
+    parent_cue = None
+    
+    regex_timestamp = r'(\d+):(\d+):(\d+)'
+    compiled_regex_timestamp = re.compile(regex_timestamp)
+    
+    def __init__(self, track_id, path = None, track_type = None, blocksize = None, parent_cue = None):
+        self.id = track_id
+        self.indexes = {}
+        if path:
+            self.path = path
+        if blocksize:
+            self.blocksize = blocksize
+        if track_type:
+            self.type = track_type
+            self.is_audio_track = self.type.casefold() == 'AUDIO'.casefold()
+            self.blocksize = Blocksizes[track_type.casefold]
+        if parent_cue:
+            self.parent_cue = parent_cue
+        
+    
+    def is_audio_track(self):
+        return self.is_audio_track
+    
+    def is_data_track(self):
+        return not self.is_audio_track
+    
+    def add_index(self, string):
+        index = Index(string)
+        self.indexes[index.id] = index
+        
+    def check_if_exists(self):
+        if self.path and not osp.exists(self.path):
+            logging.error('******** ERROR EN FICHERO ENLAZADO ********')
+            logging.error('ERROR !!!! - La pista ' + self.id + ' del CUE ' + self.parent_cue.path + ' hace referencia a un fichero que no existe')
+            logging.error('Compruebe que el fichero "' + self.path + '" existe y está en la misma ruta que el CUE. ¿Es posible que lo haya renombrado?')
+            logging.error('Recuerde que el nombre del fichero .BIN debe coincidir con el nombre que aparece dentro del fichero .CUE')
+            logging.error('****** FIN ERROR EN FICHERO ENLAZADO ******')
+            return False
+        else:
+            return True
+            
+
+class CueFile:
+    path = None
+    blocksize = None
+    bins = None
+    tracks = None
+    has_audio_tracks = False
+    has_data_tracks = False
+    is_processable = True
+    
+    regex_file_line = r'(?:FILE|file|File) (?:\'|\")(.*)(?:\'|\").*'
+    regex_track_line = r'(?:TRACK|track|Track) (\d+) (\S)'
+    regex_index_line = r'(?:INDEX|index|Index) (\d+ \d+:\d+:\d+)'
+    compiled_regex_file_line = re.compile(regex_file_line)
+    compiled_regex_track_line = re.compile(regex_track_line)
+    compiled_regex_index_line = re.compile(regex_index_line)
+    
+    def __init__(self, cue = None):
+        self.tracks = []
+        self.bins = []
+        if cue:
+            self.path = cue
+            self.parse_cue(cue)
+    
+    def is_monofile_multitrack(self):
+        return len(self.bins) < len(self.tracks)
+    
+    def parse_cue(self, cue):
+        """
+        Parsea un fichero cue e inicializa la estructura de datos
+    
+        Parameters
+        ----------
+        cue : str
+            El cue a procesar
+
+        """
+    
+        self.path = cue
+        cue_contents = None
+        last_bin = None
+        last_track = None
+        with open(cue, "r") as file:
+            cue_contents = file.read().split('\n')
+
+        for line in cue_contents.split('\n'):
+        
+            # ¿Es linea de fichero?    
+            results = re.search(self.regex_file_line, line)
+            if results:
+                last_bin = Bin(os.path.join(os.path.dirname(cue), results.group(1)))
+                self.bins.append(last_bin)
+                
+            # ¿Es linea de pista?
+            results = re.search(self.regex_track_line, line)
+            if results:
+                last_track = Track(path=last_bin, track_id=results.group(1), track_type = results.group(2), parent_cue = self)
+                last_track.check_if_exists()
+                self.tracks.append(last_track)
+                if last_track.blocksize and not self.blocksize:
+                    self.blocksize = last_track.blocksize
+                self.has_audio_tracks |= last_track.is_audio_track()
+              
+            # ¿Es linea de indice?
+            results = re.search(self.regex_index_line, line)
+            if results:
+                last_track.add_index(results.group(1))
+
+        if self.is_processable():
+            if len(self.bins) == len(self.tracks):
+                logging.debug("El CUE [ " + self.path + " ] tiene tantas pistas como ficheros, se continua de forma normal.")
+            else:
+                if len(self.bins) != 1:
+                    self.is_processable = False
+                    msg = "El CUE [ " + self.path + " ] es multifichero, pero no se corresponde con las listas. Este caso no está contemplado, lo siento."
+                    logging.debug(msg)
+                else:
+                    logging.debug("El CUE [ " + self.path + " ] es multipista con un solo fichero. Se partirá antes de continuar.")
+                    bin_file = self.bins[0]
+                    max_offset = bin_file.size // self.globalBlocksize
+                    logging.debug("El fichero " + bin_file.path + " tiene " + str(max_offset) + " frames. Empezamos a mapear con las pistas de audio desde el final.")
+                    actual_offset = max_offset
+                    for track in reversed(bin_file.tracks):
+                        track.tracksize = (actual_offset - track.indexes["01"].length_in_frames) * track.blocksize
+                        actual_offset = track.indexes["01"].length_in_frames
+
+        logging.debug("Resultados del parseo del fichero: " + cue)
+        logging.debug("Bins: " + str(len(self.bins)))
+        logging.debug("Pistas: " + str(len(self.tracks)))
+        logging.debug("Se puede comprimir: " + ( "si" if self.is_processable() else "NO"))
+        
+    def is_processable(self):
+        if self.is_processable:
+            if len(self.bins) == 0:
+                self.is_processable = False
+                msg = "No se encontraron ficheros BIN en el CUE [ " + self.path + " ]. No se realizará ninguna operación sobre este CUE."
+                logging.debug(msg)
+            else:
+                for track in self.tracks:
+                    self.is_processable &= track.check_if_exists()
+                if not self.is_processable:
+                    msg = "No se pueden procesar ficheros CUE para el cual no dispone de todos los archivos BIN enlazados"
+                    logging.debug(msg)
+        return self.is_processable
+    
+    def copy_to_dir(self, path):
+        logging.debug('Copiando juego ' + self.path + ' al directorio ' + path)
+        for bin_file in self.bins:
+            copy(os.path.join(path, bin_file.path), path)
+        copy(self.path, path)
+        new_cue = copy.copy(self)
+        new_cue.path = os.path.join(path, basename(self.path))
+        return new_cue
+
+    def get_audio_bins(self):
+        files_to_process = []
+        for track in self.tracks:
+            if track.is_audio_track():
+                files_to_process.append(track)
+        return files_to_process
+    
+    def split_tracks(self):
+        """
+        Separa el bin único en multiples pistas, creando un nuevo cue, el cual devuelve.
+    
+        Returns
+        -------
+        CueFile
+            El nuevo cue
+    
+        """
+        new_cue = copy.copy(self)
+        new_cue.tracks = []
+        if self.is_monofile_multitrack():
+            # use calculated sectors, read the same amount, start new file when equal
+            for track in self.tracks:
+                source_bin = track.path
+                written = 0
+                chunksize = 1024 * 1024
+                target_bin = basename(track.path) + ' (Track ' + str(track.num) + ').bin'
+                new_track = copy.copy(track)
+                new_track.path = target_bin
+                with open(source_bin.path, 'rb') as source_file:
+                    with open(target_bin, 'wb') as target_file:
+                        while written != track.tracksize:
+                            if chunksize + written > track.tracksize:
+                                chunksize = track.tracksize - written
+                            chunk = source_file.read(chunksize)
+                            target_file.write(chunk)
+                            written += chunksize
+        
 
 class BinCueMinimizer:
 
@@ -524,6 +793,36 @@ class BinCueMinimizer:
             self.run(self.base_cue_to_chd_command.split(), {'%INPUT': source, '%OUTPUT': target})
             files_to_process.append(target)
         return files_to_process
+    
+        def minimise_cue(self, cue):
+            if cue.is_processable():
+                # Tanto si se puede comprimir, como si no, haremos la version normal
+                self.from_cue_to_chd(cue.path)
+                if cue.has_audio_tracks:
+                    self.create_working_dir()
+                    try:
+                        cue = cue.copy_to_dir()
+                        self.enter_working_dir()
+                        try:
+                            if cue.is_monofile_multitrack():
+                                continue
+                            audio_bins = []
+                            for audio_bin in cue.get_audio_bins():
+                                audio_bins.append(audio_bin.path)
+                            wavs = self.from_bin_to_wav(audio_bins)
+                            lossy_wavs = self.apply_lossywav_to_wav(wavs)
+                            compliant_lossy_wavs = self.fix_lossywav_compliance(lossy_wavs)
+                            self.delete_files(lossy_wavs)
+                            lossywav_bins = self.from_wav_to_bin(compliant_lossy_wavs)
+                            self.delete_files(compliant_lossy_wavs)
+                            lossywav_chds = self.from_cue_to_chd(cue.path, suffix="lossy")
+                            self.delete_files(lossywav_bins)
+                            self.copy_files_to_original_dir(lossywav_chds)
+                        finally:
+                            self.exit_working_dir()
+                    finally:
+                        self.delete_working_dir()
+
 
 def main():
     minimizer = BinCueMinimizer()
@@ -565,7 +864,56 @@ def main():
     # Los juegos sin pistas se pueden procesar del tiron
     if cues_without_audio_tracks:
         minimizer.from_cue_to_chd(cues_without_audio_tracks)
-            
+        
+        
+    cues = minimizer.get_cues()
+    parsed_cues = []
+    for cue in cues:
+        parsed_cues.append(CueFile(cue))
 
+    for cue in parsed_cues:
+        if cue.is_processable():
+            minimizer.create_working_dir()
+            try:
+                cue = cue.copy_to_dir()
+                minimizer.enter_working_dir()
+                try:
+                    if cue.has_audio_tracks:
+                        if cue.is_monofile_multitrack():
+                            continue
+                        
+                        audio_bins = cue.get_audio_bins()
+                        minimizer.apply_lossywav_to_bins
+                        wavs = minimizer.from_bin_to_wav(audio_bins)
+                        lossy_wavs = minimizer.apply_lossywav_to_wav(wavs)
+                        compliant_lossy_wavs = minimizer.fix_lossywav_compliance(lossy_wavs)
+                        minimizer.delete_files(lossy_wavs)
+                        lossywav_bins = minimizer.from_wav_to_bin(compliant_lossy_wavs)
+                        minimizer.delete_files(compliant_lossy_wavs)
+                        lossywav_chds = minimizer.from_cue_to_chd(temp_cues_with_audio_tracks, suffix="lossy")
+                        minimizer.delete_files(lossywav_bins)
+                    
+                    
+                    
+                    minimizer.from_cue_to_chd(cue.path)
+                finally:
+                    minimizer.exit_working_dir()
+            finally:
+                minimizer.delete_working_dir()
+'''
+obten los cues
+procesa los cues
+para cada cue
+  si es procesable
+    metete en la carpeta de trabajo
+    si no tiene audio
+      convierte
+    si si tiene audio
+      si es monofichero
+        separalo
+      procesa los wavs
+      convierte
+'''
+                
 if __name__ == '__main__':
     main()
